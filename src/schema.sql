57ea12821ed331be382854f5586fd3b73e7e7e24
Yusuf-Patel7 <Yusuf.Patel@ada.ac.uk>
2025-02-04 15:09:42 +0000

    Data Imported Successfully

--- src/schema.sql
+++ src/schema.sql
@@ -17,145 +17,149 @@
 -- TODO: Create the following tables:
 -- 1. locations 
 CREATE TABLE locations (
-    location_id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
+    location_id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
     address TEXT NOT NULL,
-    phone_number VARCHAR(20),
-    email VARCHAR(100),
-    opening_hours TEXT
+    phone_number NOT NULL CHECK(phone_number LIKE '555-____'),
+    email TEXT NOT NULL,
+    opening_hours TEXT NOT NULL
 );
 -- 2. members
 CREATE TABLE members (
-    member_id SERIAL PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(100),
-    phone_number VARCHAR(20),
-    date_of_birth DATE,
-    join_date DATE NOT NULL,
-    emergency_contact_name VARCHAR(100),
-    emergency_contact_phone VARCHAR(20)
+    member_id INTEGER PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    phone_number TEXT NOT NULL CHECK(phone_number LIKE '555-____'),
+    date_of_birth TEXT NOT NULL,
+    join_date TEXT NOT NULL,
+    emergency_contact_name TEXT NOT NULL,
+    emergency_contact_phone TEXT NOT NULL CHECK(phone_number LIKE '555-____')
 );
 -- 3. staff
 CREATE TABLE staff (
-    staff_id SERIAL PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(100),
-    phone_number VARCHAR(20),
-    position VARCHAR(20) NOT NULL CHECK (position IN ('Trainer', 'Manager', 'Receptionist')),
-    hire_date DATE NOT NULL,
-    location_id INT NOT NULL,
-    FOREIGN KEY (location_id) REFERENCES locations(location_id)
+    staff_id INTEGER PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    phone_number TEXT NOT NULL CHECK(phone_number LIKE '555-____'),
+    position TEXT NOT NULL CHECK (position IN ('Trainer', 'Manager', 'Receptionist', 'Maintenance')),
+    hire_date TEXT NOT NULL,
+    location_id INTEGER,
+    FOREIGN KEY (location_id) REFERENCES locations(location_id) ON UPDATE CASCADE
+    ON DELETE SET NULL
 );
 -- 4. equipment
 CREATE TABLE equipment (
-    equipment_id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    type VARCHAR(20) NOT NULL CHECK (type IN ('Cardio', 'Strength')),
-    purchase_date DATE,
-    last_maintenance_date DATE,
-    next_maintenance_date DATE,
-    location_id INT NOT NULL,
-    FOREIGN KEY (location_id) REFERENCES locations(location_id)
+    equipment_id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    type TEXT NOT NULL CHECK (type IN ('Cardio', 'Strength')),
+    purchase_date TEXT NOT NULL,
+    last_maintenance_date TEXT NOT NULL CHECK(last_maintenance_date >= purchase_date),
+    next_maintenance_date TEXT NOT NULL CHECK(next_maintenance_date >= last_maintenance_date),
+    location_id INTEGER,
+    FOREIGN KEY (location_id) REFERENCES locations(location_id) ON DELETE CASCADE
 );
 
 
 -- 5. classes
 CREATE TABLE classes (
-    class_id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
+    class_id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
     description TEXT,
-    capacity INT NOT NULL CHECK (capacity > 0),
-    duration INTERVAL NOT NULL,
-    location_id INT NOT NULL,
-    FOREIGN KEY (location_id) REFERENCES locations(location_id)
+    capacity INTEGER NOT NULL,
+    duration INTEGER NOT NULL,
+    location_id INTEGER,
+    FOREIGN KEY (location_id) REFERENCES locations(location_id) ON DELETE CASCADE
 );
 -- 6. class_schedule
 CREATE TABLE class_schedule (
-    schedule_id SERIAL PRIMARY KEY,
-    class_id INT NOT NULL,
-    date DATE NOT NULL,
-    staff_id INT NOT NULL,
-    start_time TIME NOT NULL,
-    end_time TIME NOT NULL,
-    FOREIGN KEY (class_id) REFERENCES classes(class_id),
-    FOREIGN KEY (staff_id) REFERENCES staff(staff_id)
+    schedule_id INTEGER PRIMARY KEY,
+    class_id INTEGER,
+    staff_id INTEGER,
+    start_time TEXT NOT NULL,
+    end_time TEXT NOT NULL CHECK(end_time > start_time),
+    FOREIGN KEY (class_id) REFERENCES classes(class_id) ON DELETE CASCADE,
+    FOREIGN KEY (staff_id) REFERENCES staff(staff_id) ON DELETE SET NULL
 );
 
 -- 7. memberships
-CREATE TABLE membership (
-    membership_id SERIAL PRIMARY KEY,
-    member_id INT NOT NULL,
-    type VARCHAR(50) NOT NULL,
-    start_date DATE NOT NULL,
-    end_date DATE,
-    status VARCHAR(10) NOT NULL CHECK (status IN ('Active', 'Inactive')),
-    FOREIGN KEY (member_id) REFERENCES members(member_id)
+CREATE TABLE memberships (
+    membership_id INTEGER PRIMARY KEY,
+    member_id INTEGER,
+    type TEXT NOT NULL CHECK (type IN ('Basic', 'Premium')),
+    start_date TEXT NOT NULL,
+    end_date TEXT NOT NULL,
+    status TEXT NOT NULL CHECK (status IN ('Active', 'Inactive')),
+    FOREIGN KEY (member_id) REFERENCES members(member_id) ON DELETE CASCADE
 );
 -- 8. attendance
 CREATE TABLE attendance (
-    attendance_id SERIAL PRIMARY KEY,
-    member_id INT NOT NULL,
-    location_id INT NOT NULL,
-    check_in_time TIMESTAMP NOT NULL,
-    check_out_time TIMESTAMP,
-    FOREIGN KEY (member_id) REFERENCES members(member_id),
-    FOREIGN KEY (location_id) REFERENCES locations(location_id)
+    attendance_id INTEGER PRIMARY KEY,
+    member_id INTEGER,
+    location_id INTEGER,
+    check_in_time TEXT NOT NULL,
+    check_out_time TEXT NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES members(member_id) ON DELETE CASCADE,
+    FOREIGN KEY (location_id) REFERENCES locations(location_id) ON DELETE CASCADE
 );
 -- 9. class_attendance
 CREATE TABLE class_attendance (
-    class_attendance_id SERIAL PRIMARY KEY,
-    schedule_id INT NOT NULL,
-    member_id INT NOT NULL,
-    attendance_status VARCHAR(20) NOT NULL CHECK (attendance_status IN ('Registered', 'Attended', 'Unattended')),
-    FOREIGN KEY (schedule_id) REFERENCES class_schedule(schedule_id),
-    FOREIGN KEY (member_id) REFERENCES members(member_id)
+    class_attendance_id INTEGER PRIMARY KEY,
+    schedule_id INTEGER,
+    member_id INTEGER,
+    attendance_status TEXT NOT NULL CHECK (attendance_status IN ('Registered', 'Attended', 'Unattended')),
+    FOREIGN KEY (schedule_id) REFERENCES class_schedule(schedule_id) ON DELETE CASCADE,
+    FOREIGN KEY (member_id) REFERENCES members(member_id) ON DELETE CASCADE
 );
 
 -- 10. payments
 CREATE TABLE payments (
-    payment_id SERIAL PRIMARY KEY,
-    member_id INT NOT NULL,
-    amount DECIMAL(10,2) NOT NULL,
-    payment_date DATE NOT NULL,
-    payment_method VARCHAR(20) NOT NULL CHECK (payment_method IN ('Credit Card', 'Bank Transfer', 'PayPal')),
-    payment_type VARCHAR(30) NOT NULL CHECK (payment_type IN ('Monthly membership fee', 'Day pass')),
-    FOREIGN KEY (member_id) REFERENCES members(member_id)
+    payment_id INTEGER PRIMARY KEY,
+    member_id INTEGER,
+    amount REAL NOT NULL CHECK(amount > 0),
+    payment_date TEXT NOT NULL,
+    payment_method TEXT NOT NULL CHECK (payment_method IN ('Credit Card', 'Bank Transfer', 'PayPal','Cash')),
+    payment_type TEXT NOT NULL CHECK (payment_type IN ('Monthly membership fee', 'Day pass')),
+    FOREIGN KEY (member_id) REFERENCES members(member_id) ON DELETE SET NULL
 );
 -- 11. personal_training_sessions
 CREATE TABLE personal_training_sessions (
-    session_id SERIAL PRIMARY KEY,
-    member_id INT NOT NULL,
-    staff_id INT NOT NULL,
-    session_date DATE NOT NULL,
-    start_time TIME NOT NULL,
-    end_time TIME NOT NULL,
+    session_id INTEGER PRIMARY KEY,
+    member_id INTEGER,
+    staff_id INTEGER,
+    session_date TEXT NOT NULL,
+    start_time TEXT NOT NULL,
+    end_time TEXT NOT NULL,
     notes TEXT,
-    FOREIGN KEY (member_id) REFERENCES members(member_id),
-    FOREIGN KEY (staff_id) REFERENCES staff(staff_id)
+    FOREIGN KEY (member_id) REFERENCES members(member_id) ON DELETE CASCADE,
+    FOREIGN KEY (staff_id) REFERENCES staff(staff_id) ON DELETE SET NULL
 );
 -- 12. member_health_metrics
 CREATE TABLE member_health_metrics (
-    metric_id SERIAL PRIMARY KEY,
-    member_id INT NOT NULL,
-    measurement_date DATE NOT NULL,
-    weight DECIMAL(5,2),
-    body_fat_percentage DECIMAL(5,2),
-    muscle_mass DECIMAL(5,2),
-    bmi DECIMAL(5,2),
-    FOREIGN KEY (member_id) REFERENCES members(member_id)
+    metric_id INTEGER PRIMARY KEY,
+    member_id INTEGER,
+    measurement_date TEXT NOT NULL,
+    weight REAL NOT NULL,
+    body_fat_percentage REAL,
+    muscle_mass REAL,
+    bmi REAL,
+    FOREIGN KEY (member_id) REFERENCES members(member_id) ON DELETE CASCADE
 );
 -- 13. equipment_maintenance_log
 CREATE TABLE equipment_maintenance_log (
-    log_id SERIAL PRIMARY KEY,
-    equipment_id INT NOT NULL,
-    maintenance_date DATE NOT NULL,
-    description TEXT,
-    staff_id INT NOT NULL,
-    FOREIGN KEY (equipment_id) REFERENCES equipment(equipment_id),
-    FOREIGN KEY (staff_id) REFERENCES staff(staff_id)
+    log_id INTEGER PRIMARY KEY,
+    equipment_id INTEGER,
+    maintenance_date TEXT NOT NULL,
+    description TEXT NOT NULL,
+    staff_id INTEGER,
+    FOREIGN KEY (equipment_id) REFERENCES equipment(equipment_id) ON DELETE CASCADE,
+    FOREIGN KEY (staff_id) REFERENCES staff(staff_id) ON DELETE SET NULL
 );
 
 -- After creating the tables, you can import the sample data using:
 -- `.read data/sample_data.sql` in a sql file or `npm run import` in the terminal
+
+-- Make sure to delete existing database first otherwise setup will cause an error
+--.read src/schema.sql 
+--.read scripts/sample_data.sql
