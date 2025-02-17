63804598a2d2de6971fcf72b6ce85f7705786161
Yusuf-Patel7 <Yusuf.Patel@ada.ac.uk>
2025-02-17 15:47:51 +0000

    payments done

--- src/schema.sql
+++ src/schema.sql
@@ -5,8 +5,9 @@
 .mode column
 
 -- Enable foreign key support
-
+PRAGMA foreign_keys = ON;
 -- Create your tables here
+
 -- Example:
 -- CREATE TABLE table_name (
 --     column1 datatype,
@@ -18,33 +19,33 @@
 -- 1. locations 
 CREATE TABLE locations (
     location_id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    address TEXT NOT NULL,
-    phone_number NOT NULL CHECK(phone_number LIKE '555-____'),
-    email TEXT NOT NULL,
-    opening_hours TEXT NOT NULL
+    name VARCHAR(50) NOT NULL,
+    address VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(20) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    opening_hours VARCHAR(20) NOT NULL
 );
 -- 2. members
 CREATE TABLE members (
     member_id INTEGER PRIMARY KEY,
-    first_name TEXT NOT NULL,
-    last_name TEXT NOT NULL,
-    email TEXT NOT NULL,
-    phone_number TEXT NOT NULL CHECK(phone_number LIKE '555-____'),
-    date_of_birth TEXT NOT NULL,
-    join_date TEXT NOT NULL,
-    emergency_contact_name TEXT NOT NULL,
-    emergency_contact_phone TEXT NOT NULL CHECK(phone_number LIKE '555-____')
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(20) NOT NULL,
+    date_of_birth VARCHAR(20) NOT NULL,
+    join_date VARCHAR(20) NOT NULL,
+    emergency_contact_name VARCHAR(50) NOT NULL,
+    emergency_contact_phone VARCHAR(20) NOT NULL 
 );
 -- 3. staff
 CREATE TABLE staff (
     staff_id INTEGER PRIMARY KEY,
-    first_name TEXT NOT NULL,
-    last_name TEXT NOT NULL,
-    email TEXT NOT NULL,
-    phone_number TEXT NOT NULL CHECK(phone_number LIKE '555-____'),
-    position TEXT NOT NULL CHECK (position IN ('Trainer', 'Manager', 'Receptionist', 'Maintenance')),
-    hire_date TEXT NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(20) NOT NULL,
+    position VARCHAR(20) NOT NULL CHECK (position IN ('Trainer', 'Manager', 'Receptionist', 'Maintenance')),
+    hire_date VARCHAR(20) NOT NULL,
     location_id INTEGER,
     FOREIGN KEY (location_id) REFERENCES locations(location_id) ON UPDATE CASCADE
     ON DELETE SET NULL
@@ -52,11 +53,11 @@ CREATE TABLE staff (
 -- 4. equipment
 CREATE TABLE equipment (
     equipment_id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    type TEXT NOT NULL CHECK (type IN ('Cardio', 'Strength')),
-    purchase_date TEXT NOT NULL,
-    last_maintenance_date TEXT NOT NULL CHECK(last_maintenance_date >= purchase_date),
-    next_maintenance_date TEXT NOT NULL CHECK(next_maintenance_date >= last_maintenance_date),
+    name VARCHAR(50) NOT NULL,
+    type VARCHAR(20) NOT NULL CHECK (type IN ('Cardio', 'Strength')),
+    purchase_date VARCHAR(20) NOT NULL,
+    last_maintenance_date VARCHAR(20) NOT NULL CHECK(last_maintenance_date >= purchase_date),
+    next_maintenance_date VARCHAR(20) NOT NULL CHECK(next_maintenance_date >= last_maintenance_date),
     location_id INTEGER,
     FOREIGN KEY (location_id) REFERENCES locations(location_id) ON DELETE CASCADE
 );
@@ -65,8 +66,8 @@ CREATE TABLE equipment (
 -- 5. classes
 CREATE TABLE classes (
     class_id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    description TEXT,
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(255),
     capacity INTEGER NOT NULL,
     duration INTEGER NOT NULL,
     location_id INTEGER,
@@ -77,8 +78,8 @@ CREATE TABLE class_schedule (
     schedule_id INTEGER PRIMARY KEY,
     class_id INTEGER,
     staff_id INTEGER,
-    start_time TEXT NOT NULL,
-    end_time TEXT NOT NULL CHECK(end_time > start_time),
+    start_time VARCHAR(20) NOT NULL,
+    end_time VARCHAR(20) NOT NULL CHECK(end_time > start_time),
     FOREIGN KEY (class_id) REFERENCES classes(class_id) ON DELETE CASCADE,
     FOREIGN KEY (staff_id) REFERENCES staff(staff_id) ON DELETE SET NULL
 );
@@ -87,10 +88,10 @@ CREATE TABLE class_schedule (
 CREATE TABLE memberships (
     membership_id INTEGER PRIMARY KEY,
     member_id INTEGER,
-    type TEXT NOT NULL CHECK (type IN ('Basic', 'Premium')),
-    start_date TEXT NOT NULL,
-    end_date TEXT NOT NULL,
-    status TEXT NOT NULL CHECK (status IN ('Active', 'Inactive')),
+    type VARCHAR(7) NOT NULL CHECK (type IN ('Basic', 'Premium')),
+    start_date VARCHAR(20) NOT NULL,
+    end_date VARCHAR(20) NOT NULL,
+    status VARCHAR(10) NOT NULL CHECK (status IN ('Active', 'Inactive')),
     FOREIGN KEY (member_id) REFERENCES members(member_id) ON DELETE CASCADE
 );
 -- 8. attendance
@@ -98,8 +99,8 @@ CREATE TABLE attendance (
     attendance_id INTEGER PRIMARY KEY,
     member_id INTEGER,
     location_id INTEGER,
-    check_in_time TEXT NOT NULL,
-    check_out_time TEXT NOT NULL,
+    check_in_time VARCHAR(20) NOT NULL,
+    check_out_time VARCHAR(20) NOT NULL,
     FOREIGN KEY (member_id) REFERENCES members(member_id) ON DELETE CASCADE,
     FOREIGN KEY (location_id) REFERENCES locations(location_id) ON DELETE CASCADE
 );
@@ -108,7 +109,7 @@ CREATE TABLE class_attendance (
     class_attendance_id INTEGER PRIMARY KEY,
     schedule_id INTEGER,
     member_id INTEGER,
-    attendance_status TEXT NOT NULL CHECK (attendance_status IN ('Registered', 'Attended', 'Unattended')),
+    attendance_status VARCHAR(10) NOT NULL CHECK (attendance_status IN ('Registered', 'Attended', 'Unattended')),
     FOREIGN KEY (schedule_id) REFERENCES class_schedule(schedule_id) ON DELETE CASCADE,
     FOREIGN KEY (member_id) REFERENCES members(member_id) ON DELETE CASCADE
 );
@@ -118,9 +119,9 @@ CREATE TABLE payments (
     payment_id INTEGER PRIMARY KEY,
     member_id INTEGER,
     amount REAL NOT NULL CHECK(amount > 0),
-    payment_date TEXT NOT NULL,
-    payment_method TEXT NOT NULL CHECK (payment_method IN ('Credit Card', 'Bank Transfer', 'PayPal','Cash')),
-    payment_type TEXT NOT NULL CHECK (payment_type IN ('Monthly membership fee', 'Day pass')),
+    payment_date VARCHAR(20) NOT NULL,
+    payment_method VARCHAR(15) NOT NULL CHECK (payment_method IN ('Credit Card', 'Bank Transfer', 'PayPal','Cash')),
+    payment_type VARCHAR(25) NOT NULL CHECK (payment_type IN ('Monthly membership fee', 'Day pass')),
     FOREIGN KEY (member_id) REFERENCES members(member_id) ON DELETE SET NULL
 );
 -- 11. personal_training_sessions
@@ -128,10 +129,10 @@ CREATE TABLE personal_training_sessions (
     session_id INTEGER PRIMARY KEY,
     member_id INTEGER,
     staff_id INTEGER,
-    session_date TEXT NOT NULL,
-    start_time TEXT NOT NULL,
-    end_time TEXT NOT NULL,
-    notes TEXT,
+    session_date VARCHAR(10) NOT NULL,
+    start_time VARCHAR(10) NOT NULL,
+    end_time VARCHAR(10) NOT NULL,
+    notes VARCHAR(255),
     FOREIGN KEY (member_id) REFERENCES members(member_id) ON DELETE CASCADE,
     FOREIGN KEY (staff_id) REFERENCES staff(staff_id) ON DELETE SET NULL
 );
@@ -139,7 +140,7 @@ CREATE TABLE personal_training_sessions (
 CREATE TABLE member_health_metrics (
     metric_id INTEGER PRIMARY KEY,
     member_id INTEGER,
-    measurement_date TEXT NOT NULL,
+    measurement_date VARCHAR(10) NOT NULL,
     weight REAL NOT NULL,
     body_fat_percentage REAL,
     muscle_mass REAL,
@@ -150,8 +151,8 @@ CREATE TABLE member_health_metrics (
 CREATE TABLE equipment_maintenance_log (
     log_id INTEGER PRIMARY KEY,
     equipment_id INTEGER,
-    maintenance_date TEXT NOT NULL,
-    description TEXT NOT NULL,
+    maintenance_date VARCHAR(10) NOT NULL,
+    description VARCHAR(255) NOT NULL,
     staff_id INTEGER,
     FOREIGN KEY (equipment_id) REFERENCES equipment(equipment_id) ON DELETE CASCADE,
     FOREIGN KEY (staff_id) REFERENCES staff(staff_id) ON DELETE SET NULL
